1ec0371d805affeadb32036e296803102e17a940
Naveen <naveengoyal493@gmail.com>
2025-12-11 21:41:02 +0530

    updated build

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 862, hash: '37bac5f5921bdc1acba8bc550a05529c752b7e5d63aa1753a679e1ffd0583fc9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1023, hash: 'bf582c84cf5bf0a0ca65351a3948ccabd42568760598a8846bd553c3c72f61d1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 15116, hash: '52dc61cb1c8fb6b518a032adadfc742fdae65d592dee0adefaa03566f5408755', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 851, hash: '73455fe74830347092b26935695fa34eff90b8f76139ad0fc968f502d44f1033', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1012, hash: '962fa5544bdc0a08725a00562e597eb304725f2920779bc83acc79450a2f7c0e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 16338, hash: '80b5560f94f86c289aa7a0c293662e512ac2cff5e6193c1f289c272014e8fe2c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-4VJCOZCD.css': {size: 824, hash: 'g3PpPAqstqM', text: () => import('./assets-chunks/styles-4VJCOZCD_css.mjs').then(m => m.default)}
   },
 };
